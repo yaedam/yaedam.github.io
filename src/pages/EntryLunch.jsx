72c45349a2c254e45b5c21713d846e5b6bc16c3d
yaedam <dddeee02@gmail.com>
2026-01-15 14:50:04 +0900

    fix: Remove conflicting src/index.css imports from entry files

--- src/pages/EntryLunch.jsx
+++ src/pages/EntryLunch.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Lunch from './Lunch';
-import '../index.css';
+
 
 ReactDOM.createRoot(document.getElementById('lunch-root')).render(
     <React.StrictMode>
--- src/pages/EntryWorkLog.jsx
+++ src/pages/EntryWorkLog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WorkLog from './WorkLog';
-import '../index.css';
+
 
 ReactDOM.createRoot(document.getElementById('worklog-root')).render(
     <React.StrictMode>
